72ed64bcb82f829e1b69385d2a17d57724da9afb
Dragenst1 <37278819+Dragenst1@users.noreply.github.com>
2018-08-05 11:37:12 -0700

    Init- 8/5/18

---
+++ dottGRIP Prodigy/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dottGRIP_Prodigy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
